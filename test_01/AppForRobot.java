339a05d135a9f38ac07e28639537f8884c87741f
Talieva Elena <erulen@mail.ru>
2019-10-14 14:47:43 +0300

    01 done + javaDoc

--- test_01/AppForRobot.java
+++ test_01/AppForRobot.java
@@ -3,17 +3,23 @@ package test_01;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Приложения для форматированного вывода списка слов.
+ * Вывести все непустые слова через запятую, последнее через 'и'.
+ * Пример: "Кошка, Собака, Крокодил, Муха и Летучая мышь!"
+ */
+
 public class AppForRobot {
 
     public static void main(String[] args) {
-        List <String> speach = new ArrayList<>();
+        List<String> speach = new ArrayList<>();
+        speach.add("Лебедь");
+        speach.add(null);
+        speach.add("Рак");
+        speach.add("Щука");
+        speach.add(null);
         speach.add("Кошка");
-        speach.add("Собака");
-        speach.add(null);
-        speach.add("Крокодил");
-        speach.add(null);
-        speach.add("Муха");
-        speach.add("Летучая мышь");
+        speach.add("Барбос");
 
         Robot robo = new Robot();
         robo.talk(speach);
--- test_01/Robot.java
+++ test_01/Robot.java
@@ -2,14 +2,25 @@ package test_01;
 
 import java.util.List;
 
+/**
+ * Класс робот с методом форматированного вывода списка слов
+ */
+
 class Robot {
+
+    /**
+     * Формат вывода: все !null слова через запятую, последнее через 'и'.
+     * Пример: "Кошка, Собака, Крокодил, Муха и Летучая мышь!"
+     *
+     * @param speach список слов для форматированного вывода
+     */
     void talk(List<String> speach) {
         for (int i = 0; i < speach.size(); i++) {
             if (speach.get(i) != null) {
                 if (i == 0) {
                     System.out.print(speach.get(i));
                 } else {
-                    if (i == speach.size()-1) {
+                    if (i == speach.size() - 1) {
                         System.out.print(" и " + speach.get(i) + "!");
                     } else {
                         System.out.print(", " + speach.get(i));
